fae589a38feaa965d90912807042ef797756deb0
Argho Das <aiub2016@yahoo.com>
2020-04-08 16:50:17 +0600

    Source Code Files
    This is a C++ code solving a acm case in codechef.

---
+++ Vowel and Consonant.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+char C;
+cin>>C;
+    if (C== 'A'||C== 'E'||C== 'I'|| C=='O'|| C=='U'){
+    cout<<"vowel"<<endl;
+}
+else
+{
+    cout<<"consonant"<<endl;
+}
+
+return 0;
+}
